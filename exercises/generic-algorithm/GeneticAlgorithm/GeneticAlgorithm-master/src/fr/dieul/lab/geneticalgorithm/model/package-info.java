b5d49c7a6c68785abf13f0d6f5dd9675d022c676
mahaamesha <mahaamesha@gmail.com>
2022-05-09 22:06:58 +0700

    checkpoints branch: assignment_only

--- exercises/generic-algorithm/GeneticAlgorithm/GeneticAlgorithm-master/src/fr/dieul/lab/geneticalgorithm/model/package-info.java
+++
@@ -1,3 +0,0 @@
-package fr.dieul.lab.geneticalgorithm.model;
-
-/* This package should contain all the model/business classes such as Population, Individual*/
--- test/string_indexing.py
+++
@@ -1,9 +0,0 @@
-s = 'ABCd'
-length = len(s)
-
-print(length)
-for i in range(length):
-    print(s[i], end=' ')
-print()
-
-print(s[length])
